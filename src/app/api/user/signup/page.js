35e4d6fb15dfba233c0dad3d33eb88a45a0d3f93
SutharShantanu <shantanusut2000@gmail.com>
2024-08-08 09:27:18 +0530

    Cloud sync | next-auth | chai or code

---
+++ src/app/api/user/signup/page.js
@@ -0,0 +1,12 @@
+import connect from "../../../../lib/mongoDB";
+import User from "../../../../models/user";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req, res) => {
+    await connect();
+    try {
+
+    } catch (error) {
+
+    }
+};
--- src/helpers/mailer.js
+++ src/helpers/mailer.js
@@ -1,5 +1,30 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
-export const sendEMail = async({email,emailType, userID} =>{
-    
-})
+export const sendEMail = async ({ email, emailType, userID }) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false, // Use `true` for port 465, `false` for all other ports
+            auth: {
+                user: "maddison53@ethereal.email",
+                pass: "jn7jnAPss4f63QBp6D",
+            },
+        });
+
+        const mailOptions = {
+            from: "shantanusut2000@gmail.com",
+            to: email,
+            subject:
+                emailType === "VERIFY"
+                    ? `Verify your email`
+                    : `Reset your password`,
+            html: "<b>Hello world?</b>",
+        };
+
+        const mailResponse = await transporter.sendMail(mailOptions);
+        return mailResponse;
+    } catch (error) {
+        throw new Error(error);
+    }
+};
